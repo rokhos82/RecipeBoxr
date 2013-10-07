44e2b21037d93998e684f8d3cba5499819d586d0
Justin Lane <justin.l.lane@gmail.com>
2013-10-07 17:07:18 -0600

    Created add_entry page

--- add_entry.php
+++ add_entry.php
@@ -4,10 +4,10 @@
 	<link rel="stylesheet" type="text/csss" href="main.css" />
 </head>
 <body>
-	<div class="nav"><a class="inline" href="index.php">Home</a>&gt;<a class="inline">Pantry</a></div>
+	<div class="nav"><a class="inline" href="index.php">Home</a>&gt;<a class="inline" href="pantry.php">Pantry</a>&gt;<a class="inline">Add Entry</a></div>
 	<div class="container">
 		<div class="tools">
-			<a href="add_entry.php">Add Entry</a>
+			Tools
 		</div>
 		<div class="main">Main Body</div>
 	</div>
--- index.php
+++ index.php
@@ -9,7 +9,10 @@
 		<div class="tools">
 			<a href="pantry.php">Pantries</a>
 		</div>
-		<div class="main">Main Body</div>
+		<div class="main">
+			<?php
+			?>
+		</div>
 	</div>
 	<div class="foot">Footer - may not be used</div>
 </body>
