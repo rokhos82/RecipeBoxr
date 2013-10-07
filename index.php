dc8685d2f25c58fc8676d2147e1905dc32fa5404
Justin Lane <justin.l.lane@gmail.com>
2013-10-07 11:04:52 -0600

    UI Structure started

---
+++ index.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+	<title>RecipeBoxr</title>
+	<link rel="stylesheet" type="text/csss" href="main.css" />
+</head>
+<body>
+	<div class="nav">Nav Bar</div>
+	<div class="container">
+		<div class="tools">Tool Bar</div>
+		<div class="main">Main Body</div>
+	</div>
+	<div class="foot">Footer - may not be used</div>
+</body>
+</html>
