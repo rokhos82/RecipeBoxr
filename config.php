2016c88c7078bed8f98b08f3addf015f0789d3ea
Justin Lane <justin.l.lane@gmail.com>
2013-10-14 12:43:20 -0600

    Generic config.php

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
-$mysql_host = "192.168.10.11";
+$mysql_host = "127.0.0.1";
 $mysql_user = "recipe_user";
 $mysql_pass = "b0xr";
 $mysql_db = "recipeboxr";
--- index.php
+++ index.php
@@ -23,6 +23,8 @@ $conn = mysqli_connect($mysql_host,$mysql_user,$mysql_pass,$mysql_db);
 if(mysqli_connect_errno($conn)) {
 	echo "Failed to connect to MySQL: " . mysqli_connect_error();
 }
+
+$query = "SELECT * FROM pantry;";
 ?>
 		</div>
 	</div>
