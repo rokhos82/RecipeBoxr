4af1bd26a3fc968887d8734cedba875ba76280da
Justin Lane <justin.l.lane@gmail.com>
2013-10-21 12:00:10 -0600

    Major tables are added.  Need flesh

--- setup.php
+++ setup.php
@@ -8,6 +8,22 @@ if(mysqli_connect_errno($conn)) {
 	echo "Failed to connect to MySQL: " . mysqli_connect_error();
 }
 
-$query = "CREATE TABLE pantry (pantry_id int unsigned auto_increment primary key, name );";
+/* Create user table */
+$query = "CREATE TABLE user (user_id int unsigned auto_increment primary key, fname vchar(20), lname vchar(20), uname vchar(30), email vchar(50));";
+
+/* Create pantry table */
+$query = "CREATE TABLE pantry (pantry_id int unsigned auto_increment primary key, name vchar(20), notes text, type vchar(20));";
+
+/* Create pantryEntry table */
+$query = "CREATE TABLE pantryEntry (entry_id int unsigned auto_increment primary key);";
+
+/* Create ingredientContainer table */
+$query = "CREATE TABLE ingredientContainer (container_id int unsigned auto_increment primary key);";
+
+/* Create ingredient table */
+$query = "CREATE TABLE ingredient (ingredient_id int unsigned auto_increment primary key);";
+
+/* Create recipe table */
+$query = "CREATE TABLE recipe (recipe_id int unsigned auto_increment primary key);";
 
 ?>
