466f82d9bf31e843dc621d4282d621c78580f551
Justin Lane <justin.l.lane@gmail.com>
2013-11-16 20:47:46 -0700

    Added foreign keys to tables.

--- setup.php
+++ setup.php
@@ -9,22 +9,22 @@ if(mysqli_connect_errno($conn)) {
 }
 
 /* Create user table */
-$query = "CREATE TABLE user (user_id int unsigned auto_increment primary key, fname vchar(20), lname vchar(20), uname vchar(30), email vchar(50));";
+$query = "CREATE TABLE user (user_id int unsigned auto_increment, fname vchar(20), lname vchar(20), uname vchar(30), email vchar(50), PRIMARY KEY (user_id));";
 
 /* Create pantry table */
-$query = "CREATE TABLE pantry (pantry_id int unsigned auto_increment primary key, name vchar(20), notes text, type vchar(20));";
+$query = "CREATE TABLE pantry (pantry_id int unsigned auto_increment, name vchar(20), notes text, type vchar(20), PRIMARY KEY (pantry_id));";
 
-/* Create pantryEntry table */
-$query = "CREATE TABLE pantryEntry (entry_id int unsigned auto_increment primary key, container_id int unsigned, quantity int, threshold int);";
+/* Create pantryItem table */
+$query = "CREATE TABLE pantryItem (container_id int unsigned, pantry_id int unsigned, quantity int, threshold int, PRIMARY KEY (container_id,pantry_id), FOREIGN KEY (container_id) REFERENCES foodContainer(container_id) ON UPDATE CASCADE ON DELETE RESTRICT, FOREIGN KEY (pantry_id) REFERENCES pantry(pantry_id));";
 
 /* Create foodContainer table */
-$query = "CREATE TABLE foodContainer (container_id int unsigned auto_increment primary key, name varchar(50),food_id int unsigned, size int, unit_id int unsigned;";
+$query = "CREATE TABLE foodContainer (container_id int unsigned auto_increment, name varchar(50),food_id int unsigned, size int, unit_id int unsigned, PRIMARY KEY (container_id);";
 
 /* Create food table */
-$query = "CREATE TABLE food (food_id int unsigned auto_increment primary key, name varchar(50), category varchar(50), note text);";
+$query = "CREATE TABLE food (food_id int unsigned auto_increment, name varchar(50), category varchar(50), note text, PRIMARY KEY (food_id));";
 
 /* Create recipe table */
-$query = "CREATE TABLE recipe (recipe_id int unsigned auto_increment primary key, varchar title, note text, creator int unsigned, servings int, serving_size int, serving_unit int unsigned);";
+$query = "CREATE TABLE recipe (recipe_id int unsigned auto_increment, varchar title, note text, creator int unsigned, servings int, serving_size int, serving_unit int unsigned, PRIMARY KEY(recipe_id));";
 
 /* UTILITY TABLES */
 /* Create units table and populate it. */
