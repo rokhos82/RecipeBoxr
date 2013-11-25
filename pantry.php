de1772fabff0b73f1ab9f61362d9bf7b8040c347
Justin Lane <justin.l.lane@gmail.com>
2013-11-25 12:29:19 -0700

    Added pantry lookup code to the pantry detail page

--- pantry.php
+++ pantry.php
@@ -1,10 +1,27 @@
+<?php
+require("config.php");
+
+$conn = mysqli_connect($mysql_host,$mysql_user,$mysql_pass,$mysql_db);
+if(mysqli_connect_errno($conn)) {
+	echo(mysqli_connect_error($conn));
+	die();
+}
+
+$pantry_id = mysqli_real_escape_string($conn,$_GET["id"]);
+
+$query = "SELECT * FROM pantry WHERE pantry_id=$pantry_id;";
+if($result = mysqli_query($conn,$query)) {
+	$row = mysqli_fetch_array($result);
+	$pantry_name = $row['name'];
+}
+?>
 <html>
 <head>
 	<title>RecipeBoxr</title>
 	<link rel="stylesheet" type="text/csss" href="main.css" />
 </head>
 <body>
-	<div class="nav"><a class="inline" href="index.php">Home</a>&gt;<a class="inline">Pantry - Name of Pantry Here</a></div>
+	<div class="nav"><a class="inline" href="index.php">Home</a>&gt;<a class="inline">Pantry - <?php echo($pantry_name); ?></a></div>
 	<div class="container">
 		<div class="tools">
 			<a href="add_entry.php">Add Entry</a>
