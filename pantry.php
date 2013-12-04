94710bc56726c93769e90238cea8aa7fcb6b5cfa
Justin Lane <justin.l.lane@gmail.com>
2013-12-04 11:36:38 -0700

    Fixed error in sql query for pantry items

--- pantry.php
+++ pantry.php
@@ -31,7 +31,7 @@ if($result = mysqli_query($conn,$query)) {
 			<table>
 				<tr><td>Product</td><td>Quantity</td><td>Threshold</td></tr>
 <?php
-$query = "SELECT `product`.`name AS `name`,`pantry_item`.`quantity` AS `quantity`,`pantry_item`.`threshold` AS `threshold` FROM `pantry_item` LEFT JOIN `product` ON `pantry_item`.`product_id`=`product`.`product_id` WHERE `pantry_item`.`pantry_id`=$pantry_id;";
+$query = "SELECT `product`.`name` AS `name`,`pantry_item`.`quantity` AS `quantity`,`pantry_item`.`threshold` AS `threshold` FROM `pantry_item` LEFT JOIN `product` ON `pantry_item`.`product_id`=`product`.`product_id` WHERE `pantry_item`.`pantry_id`=$pantry_id;";
 if($result = mysqli_query($conn,$query)) {
 	while($row = mysqli_fetch_array($result)) {
 		$name = $row["name"];
