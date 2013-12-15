2f49d67300f2226f3c24753f24b5fbb4b0a53451
Justin Lane <justin.l.lane@gmail.com>
2013-12-15 14:12:07 -0700

    more pantry view

--- model/header.php
+++ model/header.php
@@ -4,3 +4,8 @@
 	<link rel="stylesheet" type="text/csss" href="main.css" />
 </head>
 <body>
+	<div class="nav"><a class="inline" href="index.php">Home</a>&gt;<a class="inline">Pantry - <?php echo($pantry_name); ?></a></div>
+	<div class="container">
+		<div class="tools">
+			<a href=<?php echo "add_entry.php?pantry=$pantry_id"; ?>>Add Entry</a>
+		</div>
