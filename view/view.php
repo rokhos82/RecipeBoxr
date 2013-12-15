6c7ed3b056d7798d1fd0fa74d2f5df26af43cb8f
Justin Lane <justin.l.lane@gmail.com>
2013-12-15 16:00:29 -0700

    more cleanup and mcv work

--- view/view.php
+++ view/view.php
@@ -1,12 +1,19 @@
 <?php
 
-class rbr_view {
+class view {
 	private $controller;
 	private $model;
 
 	public function __constructor($controller,$model) {
 		$this->controller = $controller;
 		$this->model = $model;
+	}
+
+	public function output() {
+		include_once("./header.php");
+		$txt = $model->text;
+		echo("<div class=\"main\">${txt}</div>");
+		include_once("./footer.php");
 	}
 }
 
