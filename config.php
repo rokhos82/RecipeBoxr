3148be67414f6c483ea2579f44a1b2368f279da3
Justin Lane <justin.l.lane@gmail.com>
2013-12-15 20:49:27 -0700

    debugging mcv test view

--- config.php
+++ config.php
@@ -7,7 +7,23 @@ $mysql_db = "recipeboxr";
 $version = "0.0.2";
 
 $language = "en-us";
-$locale = "en_US";
 $include_path = dirname(__FILE__);
-require("$include_path/localize.php");
+require("${include_path}/localize.php");
+
+function __autoload($class_name) {
+	$include_path = dirname(__FILE__);
+	$filename = $class_name . ".php";
+	if(file_exists("${include_path}/libs/${filename}")) {
+		include("${include_path}/libs/${filename}");
+	}
+	elseif (file_exists("${include_path}/model/${filename}")) {
+		include("${include_path}/model/${filename}");
+	}
+	elseif (file_exists("${include_path}/view/${filename}")) {
+		include("${include_path}/view/${filename}");
+	}
+	elseif (file_exists("${include_path}/${filename}")) {
+		include("${include_path}/${filename}");
+	}
+}
 ?>
--- index.php
+++ index.php
@@ -1,27 +1,5 @@
 <?php
 require("config.php");
-$conn = mysqli_connect($mysql_host,$mysql_user,$mysql_pass,$mysql_db);
-
-if(mysqli_connect_errno($conn)) {
-	echo "Failed to connect to MySQL: " . mysqli_connect_error();
-	mysqli_close($conn);
-	die();
-}
-
-mysqli_close($conn);
-
-function __autoload($class_name) {
-	$filename = $class_name . ".php";
-	if(file_exists("./libs/" . $filename)) {
-		include("./libs/" . $filename);
-	}
-	elseif (file_exists("./model/" . $filename)) {
-		include("./model/" . $filename);
-	}
-	elseif (file_exists("./view/" . $filename)) {
-		include("./view/" . $filename);
-	}
-}
 
 $model = new rbrModel();
 $controller = new controller($model);
