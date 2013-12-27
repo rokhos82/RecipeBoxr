cd6741ad7ab4b234ccc0f24b6c3c55ae9dec4ae4
Justin Lane <justin.l.lane@gmail.com>
2013-12-26 20:56:42 -0700

    whitespace cleanup

--- view/header.php
+++ view/header.php
@@ -3,9 +3,9 @@ global $_GLOBALS;
 ?>
 <html>
 <head>
-	<title><?php echo $_GLOBALS["local_strings"]["title"]; ?></title>
-	<link rel="stylesheet" type="text/csss" href="main.css" />
+<title><?php echo $_GLOBALS["local_strings"]["title"]; ?></title>
+<link rel="stylesheet" type="text/csss" href="main.css" />
 </head>
 <body>
-	<div class="nav"><a class="inline" href="index.php">Home</a>&gt;<a class="inline">Pantry</a></div>
-	<div class="container">
+<div class="nav"><a class="inline" href="index.php">Home</a>&gt;<a class="inline">Pantry</a></div>
+<div class="container">
