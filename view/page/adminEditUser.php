f7de4327a8aa0a825d77ec4b1e62c76b607ecbf2
Justin Lane <justin.l.lane@gmail.com>
2014-01-05 20:49:39 -0700

    add and edit users

---
+++ view/page/adminEditUser.php
@@ -0,0 +1,12 @@
+<?php
+$details = $this->model->getUserDetails($_SESSION["userid"]);
+?>
+<p>User Edit Page</p>
+<form action="index.php" method="get">
+<label>First Name: </label><input type="text" value="<?php echo $details["fname"] ?>" name="fname" /><br />
+<label>Lane Name: </label><input type="text" value="<?php echo $details["lname"] ?>" name="lname" /><br />
+<label>User Name: </label><input type="text" value="<?php echo $details["uname"] ?>" name="uname" /><br />
+<label>Email: </label><input type="text" value="<?php echo $details["email"] ?>" name="email" /><br />
+<input type="hidden" name="action" value="adminUpdateUser" />
+<input type="submit" value="Update" />
+</form>
