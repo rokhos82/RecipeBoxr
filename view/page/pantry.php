712691dd01a78cc1d4969074cc914862cecedfc6
Justin Lane <justin.l.lane@gmail.com>
2014-01-05 19:29:17 -0700

    more on pantry page

--- view/page/pantry.php
+++ view/page/pantry.php
@@ -1,3 +1,16 @@
 <p>Manage your pantries on this page.  Create new pantries, edit existing pantries, and delete unused pantries.</p>
-<p>List of pantries goes here</p>
+<table>
+<thead>
+<tr><td colspan="2">Pantries</td></tr>
+<tr><td>Name</td><td>Notes</td></tr>
+</thead>
+<tfoot>
+</tfoot>
+<tbody>
+<?php
+$user_id = $_SESSION["userid"];
+$query = "SELECT * FROM `pantry` JOIN `user_pantry_cross` ON `pantry`.`pantry_id`=`user_pantry_cross`.`pantry_id` WHERE `user_pantry_cross`.`user_id`=${user_id};";
+?>
+</tbody>
+</table>
 <p>New pantry form goes here</p>
